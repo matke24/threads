62f5679283b5784c16559e38ad3261062709144d
Aleksa Matic <aleksa.matic10@gmail.com>
2024-08-27 16:57:05 +0200

    API prefix

--- threads-be/src/app.controller.ts
+++ threads-be/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { API_PREFIX } from './consts';
 
-@Controller()
+@Controller(API_PREFIX)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.welcome();
   }
 }
