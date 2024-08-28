58de5479944116a41bb02d56446621928a8ce218
Aleksa Matic <aleksa.matic10@gmail.com>
2024-08-28 11:58:32 +0200

    add user schema

--- threads-be/src/user/user.service.ts
+++ threads-be/src/user/user.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = new this.userModel(createUserDto);
+    return newUser.save();
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find().exec();
   }
 
   findOne(id: number) {
